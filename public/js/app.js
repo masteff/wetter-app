8226d8f7396dbb20a5c289df13bfd5a67c34244e
masteff <martin_steffan@gmx.de>
2019-06-16 19:49:20 +0200

    steup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,11 +8,11 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+    messageTwo.textContent = '' 
 
     const location = search.value;
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error){
             messageOne.textContent = data.error
